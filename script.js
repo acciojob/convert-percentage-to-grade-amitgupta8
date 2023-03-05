a5d4ff440448574a581c4bf9664c07f3207e065c
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-03-06 00:40:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-function calculateGrade(percentage) {
-  //your code here
+
+	function convertPercentageToGrade(percentage) {
+  if (isNaN(percentage) || percentage < 0) {
+    return "F";
+  }
+  const roundedPercentage = Math.round(percentage);
+  if (roundedPercentage >= 90) {
+    return "A";
+  } else if (roundedPercentage >= 80) {
+    return "B";
+  } else if (roundedPercentage >= 70) {
+    return "C";
+  } else if (roundedPercentage >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+
 }
 const percentage = parseInt(prompt("Enter Percentage."));
-alert(calculateGrade(percentage));
+alert(convertPercentageToGrade(percentage));
